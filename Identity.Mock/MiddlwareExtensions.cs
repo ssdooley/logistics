8b2370dfb1891f3caf414b9cac0e04658a9d6a8e
Jaime Still <jpstill85@gmail.com>
2018-11-19 10:27:57 -0500

    Identity.Mock created

---
+++ Identity.Mock/MiddlwareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Identity.Mock;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMockMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<MockMiddleware>();
+        }
+    }
+}
---
+++ Identity.Mock/MockMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using Logistics.Identity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Mock
+{
+    public class MockMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public MockMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext, IUserProvider userProvider, IConfiguration configuration)
+        {
+            if (!(userProvider.Initialized))
+            {
+                await userProvider.Create(configuration.GetValue<string>("CurrentUser"));
+            }
+
+            await next(httpContext);
+        }
+    }
+}
